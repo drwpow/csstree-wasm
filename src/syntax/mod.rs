73a9449bf4c5e3e46fb3fa383bd8d9281d055502
Drew Powers <drew@pow.rs>
2025-05-02 23:01:08 -0600

    More 🦀

--- src/syntax/mod.rs
+++ src/syntax/mod.rs
@@ -1,2 +1,23 @@
-pub mod at_rule;
+use std::collections::HashSet;
+
 pub mod config;
+pub mod node;
+
+// https://drafts.csswg.org/css-contain-3/#container-rule
+// The keywords `none`, `and`, `not`, and `or` are excluded from the <custom-ident> above.
+const NON_CONTAINER_NAME_KEYWORDS: HashSet<&str> = HashSet::from(["none", "and","not", "or"]);
+
+pub struct AtContainerParser {}
+
+ impl AtContainerParser {
+
+        pub fn prelude() -> () {
+            const children = this.createList();
+        }
+
+
+       fn block(nested: bool) -> () {
+
+        }
+
+}
